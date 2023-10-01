58b56de295ed94bc77efb405fc44c0c8c713317d
exdu1 <ericxdu1@gmail.com>
2023-10-01 17:51:37 -0400

    changes

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+// Modules
 import {halfOf, multiply} from './lib.js';
-//
 console.log(halfOf(84));
 console.log(multiply(21, 2));
+
+import doSomething from './doSomething.js';
+doSomething();
+
+// Modules Export Bindings and not values
+import {flag, touch} from './validator.js';
+console.log(flag);
+touch();
+console.log(flag)
+
+// Classes
